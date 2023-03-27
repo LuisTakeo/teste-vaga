322ad98962566704a1932de877f22168100bcc26
LuisTakeo <luistakeo@hotmail.com>
2023-03-26 21:56:53 -0300

    Atualizado tratativa para NaN

--- fibonacci/js/main.js
+++ fibonacci/js/main.js
@@ -5,10 +5,13 @@ const textoResposta = document.querySelector("[id=textoResposta]");
 leitorFibonacci.min = 0;
 leitorFibonacci.max = 100;
 leitorFibonacci.addEventListener("input", () => {
-    let isNegativo = leitorFibonacci.value < 0;
-    let isMaiorQue100 = leitorFibonacci.value > 100;
-    
-    if(isNegativo){
+    const isNegativo = leitorFibonacci.value < 0;
+    const isMaiorQue100 = leitorFibonacci.value > 100;
+    const isNotANumber = isNaN(parseInt(leitorFibonacci.value)); 
+    console.log(isNotANumber)   
+    if(isNotANumber){
+        leitorFibonacci.value = "";
+    }else if(isNegativo){
         leitorFibonacci.value = Math.abs(leitorFibonacci.value);
     }else if(isMaiorQue100){
         leitorFibonacci.value = 100;
@@ -25,23 +28,24 @@ while(valorNovo < 89){
     valorAnterior = valorSoma;
     valorSoma = valorNovo;
 }
-// sequenciaFibonacci.forEach(n => console.log(n))
-// console.log(sequenciaFibonacci)
 
 function verificaValorFibonacci(){
     let valorLido = parseInt(leitorFibonacci.value);
-    let isValorDeFibonacci = sequenciaFibonacci.includes(valorLido);
-    if(isValorDeFibonacci){
-        textoResposta.innerHTML = `O valor ${valorLido} faz parte da sequencia Fibonacci. <br>A sequencia é ${sequenciaFibonacci.join(" - ")}`
-    }else{
-        textoResposta.innerHTML = `O valor ${valorLido} não faz parte da sequencia Fibonacci.`
+    console.log(isNaN(valorLido))
+    if(!isNaN(valorLido)){
+        let isValorDeFibonacci = sequenciaFibonacci.includes(valorLido);
+        if(isValorDeFibonacci){
+            textoResposta.innerHTML = `O valor ${valorLido} faz parte da sequencia Fibonacci. <br>A sequencia é ${sequenciaFibonacci.join(" - ")}`;
+        }else textoResposta.innerHTML = `O valor ${valorLido} não faz parte da sequencia Fibonacci.;`
+    }else{ 
+        textoResposta.innerHTML = `Insira um valor valido.`;
     }
 }
 
-verificaFibonacci.addEventListener("click", () => {
-    verificaValorFibonacci()
+verificaFibonacci.addEventListener("click", () => verificaValorFibonacci())
+leitorFibonacci.addEventListener("keypress", (tecla) => {
+    if(tecla.key == "Enter") verificaValorFibonacci();
 })
-
 
 
 
